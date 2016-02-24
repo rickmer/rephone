38409b649fa97465d64a02168551bc89ea8dc99b
Rickmer <me@rickmer.org>
2016-02-24 19:34:02 +0100

    added profile images

--- app/views.py
+++ app/views.py
@@ -5,5 +5,5 @@ main = Blueprint('main', __name__)
 @main.route('/', methods=['GET'])
 def index():
     from .models import MitgliedDesBundestages
-    record = MitgliedDesBundestages().query.filter_by(id=1).first()
+    record = MitgliedDesBundestages().query.filter_by(id=9).first()
     return render_template('callform.html', record=record)
