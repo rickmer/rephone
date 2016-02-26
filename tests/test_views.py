ef4b10c46dea022b1c2ab1970b289a3a189d313b
Rickmer <me@rickmer.org>
2016-02-26 20:41:43 +0100

    added test for outbound twilio callback

--- tests/test_views.py
+++ tests/test_views.py
@@ -7,3 +7,10 @@ class TestViews(RephoneTest):
         with self.client:
             response = self.client.get('/')
             assert response.status_code == 200
+
+    def test_outbound(self):
+        from re import match
+        with self.client:
+            response = self.client.post('/outbound/1')
+            assert response.status_code == 200
+            assert match(r'.*<Dial><Number>\+493022772600</Number></Dial>.*', str(response.get_data()))
