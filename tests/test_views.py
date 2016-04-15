bb98decf83e92c390111f7ab97603a2e40aa90d6
Rickmer <me@rickmer.org>
2016-04-15 22:11:48 +0200

    multi campaign bug was fixed

--- tests/test_views.py
+++ tests/test_views.py
@@ -22,12 +22,22 @@ class TestViews(RephoneTest):
             assert response.status_code == 200
             assert match(r'.*<Dial><Number>\+4940123456789</Number></Dial>.*', str(response.get_data()))
 
-    def test_bias_alteration(self):
-        index_0_before = self.app.random[0][0]
-        index_1_before = self.app.random[0][1]
+    def test_bias_alteration_audience_1(self):
+        index_0_before = self.app.random[1][0]
+        index_1_before = self.app.random[1][1]
         self.app.random.add_sample(audience_id=1, respondent_id=1)
-        index_0_after = self.app.random[0][0]
-        index_1_after = self.app.random[0][1]
+        index_0_after = self.app.random[1][0]
+        index_1_after = self.app.random[1][1]
+
+        assert index_0_before == index_0_after
+        assert index_1_before == index_1_after - 1
+
+    def test_bias_alteration_audience_2(self):
+        index_0_before = self.app.random[2][0]
+        index_1_before = self.app.random[2][1]
+        self.app.random.add_sample(audience_id=2, respondent_id=752)
+        index_0_after = self.app.random[2][0]
+        index_1_after = self.app.random[2][1]
 
         assert index_0_before == index_0_after
         assert index_1_before == index_1_after - 1
