e6c94932dc30f3e53f01594c86017f48837e0bad
kirsey <erik.sy@uni-rostock.de>
2016-05-01 10:21:52 +0200

    Starting varnish from run.py

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from subprocess import run
 
 if __name__ == '__main__':
     #starting varnish webcache
-    run(["service", "varnish", "restart"])
+    run(["sudo","service", "varnish", "restart"])
     # handling command line arguments
     argparser = ArgumentParser(description='rephone - a free telephone call/callback system.')
     argparser.add_argument('--debug', action='store_true', help="Run with Interactive Debugger")
